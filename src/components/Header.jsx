dc4bb2ae316ce6f065c38fe118934c14b8477310
[Ayrak] <[m.treizebre@gmail.com]>
2023-08-06 13:16:41 +0200

    création components de base pour test et liaison api

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () =>{
+    return (
+        <h2>je suis sur le header</h2>
+    )
+}
+
+export default Header
---
+++ src/containers/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h2>je suis sur la Home</h2>
+    )
+}
+
+export default Home
