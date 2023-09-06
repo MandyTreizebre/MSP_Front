9e303e90e2472a6f0e00883515e6f630257c6478
[Ayrak] <[m.treizebre@gmail.com]>
2023-09-06 15:57:45 +0200

    modification et design de la page msp

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css'
 /*Importation des components*/
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Dentists from './components/Dentists'
+import Doctors from './components/Doctors'
+import Nurses from './components/Nurses'
+import Physiotherapists from './components/Physiotherapists'
+import Laboratory from './components/Laboratory'
+import Pharmacy from './components/Pharmacy'
+import Podiatrist from './components/Podiatrist'
+import Psychologist from './components/Psychologist'
+import Psychomotrician from './components/Psychomotrician'
+
 
 /*Importation des containers*/
 import Home from './containers/Home'
 import Msp from './containers/Msp'
-import Dentists from './containers/Dentists'
-import Doctors from './containers/Doctors'
-import Nurses from './containers/Nurses'
-import Physiotherapists from './containers/Physiotherapists'
-import Laboratory from './containers/Laboratory'
-import Pharmacy from './containers/Pharmacy'
-import Podiatrist from './containers/Podiatrist'
-import Psychologist from './containers/Psychologist'
-import Psychomotrician from './containers/Psychomotrician'
 import Contact from './containers/Contact'
 
 
--- src/components/PresentationMsp.jsx
+++ src/components/PresentationMsp.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import pict1 from "../assets/images/cabinetkines.jpg"
 
 /*Importation du Pdf à télé<charger*/
 import projetSante from '../assets/files/projet_sante.pdf'
@@ -9,20 +10,24 @@ const PresentationMsp = () => {
     return (
        
         <section className="container_presentation_msp">
-            <h1>Notre maison de santé Pluriprofessionnelle</h1>
+            <img src={pict1} alt="photo du cabinet du docteur Provent" className='pict1' />
 
-            <p>Au-delà de la structure immobilière, une Maison de Santé Pluriprofessionnelle (MSP) est avant tout une envie de 32 professionnels de santé de travailler ensemble. Cela s’est traduit par la création d’une association afin de s’organiser autour d’un projet de santé.Ce projet de santé présente un diagnostic du térritoire, un état des lieux de l’offre de soins comparativement à la région et/ou au niveau national, la mise en place d’actions des professionnels de santé pour travailler ensemble et de façon coordonné.Celui-ci a été validé et donc labellisé par les instances (ARS, CPAM, Ordres des médecins, kinés, infirmiers…). De ce fait, Varennes sur Allier dispose d’une « Maison de Santé Pluriprofessionnelle » .</p>
+            <div className='bloc_right'>
+                <h1>Notre maison de santé Pluriprofessionnelle</h1>
 
-            <Link 
-            to={projetSante}
-            download="projet-msp"
-            target="_blank"
-            rel="noreferrer"
-            >
-                <button className="general_button">
-                    Téléchargez notre projet de santé
-                </button>
-            </Link> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                <Link 
+                to={projetSante}
+                download="projet-msp"
+                target="_blank"
+                rel="noreferrer"
+                >
+                    <button className="general_button">
+                        Téléchargez notre projet de santé
+                    </button>
+                </Link> 
+            </div>
         </section>
        
     )
